9bb836d25bd1d86e5ad08e22c8ce50617aaef9a6
Kevin <alegre.kev@gmail.com>
2023-10-26 19:21:52 -0400

    Updated file structures, added file to save input files

---
+++ save_input_images.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+if os.path.exists("training-image-generator/background_images") and os.path.exists("training-image-generator/object_images"):
+    num = 1
+    while os.path.exists("training-image-generator/saved_input_images_" + str(num)):
+        num += 1
+
+    shutil.copytree("training-image-generator/background_images", "training-image-generator/saved_input_images_" + str(num) + "/saved_backgrounds")
+    shutil.copytree("training-image-generator/object_images", "training-image-generator/saved_input_images_" + str(num) + "/saved_objects")
+
+print("Saved input images as saved_input_images_" + str(num))
