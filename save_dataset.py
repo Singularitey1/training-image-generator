d207f3b56767aa199a758405610d846de3756687
Kevin <alegre.kev@gmail.com>
2023-10-26 19:28:49 -0400

    Updated file structure

--- save_dataset.py
+++ save_dataset.py
@@ -3,9 +3,9 @@ import shutil
 
 if os.path.exists("training-image-generator/result_images"):
     num = 1
-    while os.path.exists("training-image-generator/saved_dataset_" + str(num)):
+    while os.path.exists("training-image-generator/saved_datasets/saved_dataset_" + str(num)):
         num += 1
 
-    shutil.copytree("training-image-generator/result_images", "training-image-generator/saved_dataset_" + str(num))
+    shutil.copytree("training-image-generator/result_images", "training-image-generator/saved_datasets/saved_dataset_" + str(num))
 
-print("Saved dataset as saved_dataset_" + str(num))
+print("Saved dataset in saved_datasets/saved_dataset_" + str(num))
--- save_input_images.py
+++ save_input_images.py
@@ -3,10 +3,10 @@ import shutil
 
 if os.path.exists("training-image-generator/background_images") and os.path.exists("training-image-generator/object_images"):
     num = 1
-    while os.path.exists("training-image-generator/saved_input_images_" + str(num)):
+    while os.path.exists("training-image-generator/saved_input_images/saved_input_images_" + str(num)):
         num += 1
 
-    shutil.copytree("training-image-generator/background_images", "training-image-generator/saved_input_images_" + str(num) + "/saved_backgrounds")
-    shutil.copytree("training-image-generator/object_images", "training-image-generator/saved_input_images_" + str(num) + "/saved_objects")
+    shutil.copytree("training-image-generator/background_images", "training-image-generator/saved_input_images/saved_input_images_" + str(num) + "/saved_backgrounds")
+    shutil.copytree("training-image-generator/object_images", "training-image-generator/saved_input_images/saved_input_images_" + str(num) + "/saved_objects")
 
-print("Saved input images as saved_input_images_" + str(num))
+print("Saved input images in saved_input_images/saved_input_images_" + str(num))
